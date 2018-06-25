7ebe22ed31264e339b501ec825b120081affba94
Andreas Williams <andreas.williams@timewith.co.uk>
2018-06-25 18:11:12 +0100

    substitute FontAwesome icon with SVG

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import FontAwesome from '@fortawesome/react-fontawesome'
-import { faTimes } from '@fortawesome/fontawesome-pro-regular'
 import glamorous from 'glamorous'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux';
@@ -45,7 +43,7 @@ class TWPopup extends Component {
           <Scrollbars autoHeight autoHeightMin={300} autoHeightMax={window.innerHeight} id='quick-confirm-scrollbars' style={ scrollBarsStyle }>
             <CloseDIV onClick={this.handleClosePopup}>
               <CloseP>close</CloseP>
-              <FontAwesome icon={faTimes} display={closeIconStyle}/>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" style={closeIconStyle}><path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"/></svg>
             </CloseDIV>
             <ContentContainerDIV>
               { content }
@@ -193,6 +191,11 @@ const BGDIV = glamorous.div({
 const closeIconStyle = {
   display: 'inline-block',
   verticalAlign: 'top',
+  height: '24px',
+  marginLeft: '10px',
+  marginRight: '5px',
+  marginTop: '2px',
+  fill: '#63C1E8',
 }
 
 const CloseP = glamorous.p({
